e361db043d45ce8e61b0b988254566b2d0c4817d
Éderson Cássio <edersoncassiolf@outlook.com>
2023-05-01 21:10:54 -0300

    Navegador semanal

--- reservation-frontend/src/components/flight/Flight.js
+++ reservation-frontend/src/components/flight/Flight.js
@@ -1,6 +1,10 @@
+import { useState } from "react";
+
 import StepNavigator from "../stepNavigator/StepNavigator";
+import WeekSelector from "../weekSelector/WeekSelector";
 
 export default function Flight({ destinationData, step }) {
+  const [weekStart, setWeekStart] = useState(null);
 
   return (
     <>
@@ -10,6 +14,10 @@ export default function Flight({ destinationData, step }) {
         {' - '}
         {step === 'going' ? 'IDA' : 'VOLTA'}
       </h1>
+      <WeekSelector refDate={new Date()} onWeekSelected={setWeekStart} />
+      {for (let i = 0; i < 7; i++) {
+        
+      }}
     </>
   )
 }
---
+++ reservation-frontend/src/components/weekSelector/WeekSelector.js
@@ -0,0 +1,56 @@
+import { useState } from "react";
+
+import './WeekSelector.scss';
+
+function incrementDaysTo(date, days) {
+  const newDate = new Date(date);
+  newDate.setDate(newDate.getDate() + days);
+  return newDate;
+}
+
+function weekRefSunday(refDate) {
+  return incrementDaysTo(refDate, -refDate.getDay())
+}
+
+function twoZeros(n) {
+  if (n < 10) {
+    return `0${n}`;
+  }
+
+  return `${n}`;
+}
+
+function formatDate(date) {
+  return (
+    twoZeros(date.getDate()) + '/' + twoZeros(date.getMonth() + 1) + '/' +
+    date.getFullYear()
+  );
+}
+
+export default function WeekSelector({ refDate, onWeekSelected }) {
+  const [weekStart, setWeekStart] = useState(weekRefSunday(refDate));
+  
+  function changeWeek(increment) {
+    const newWeekStart = incrementDaysTo(weekStart, increment);
+    setWeekStart(newWeekStart);
+    onWeekSelected(newWeekStart);
+  }
+
+  const weekEnd = incrementDaysTo(weekStart, 6);
+
+  return (
+    <div className="WeekSelector clearFix">
+      <div className="previous" onClick={() => changeWeek(-7)}>
+        <img alt="Semana anterior" src="/arrow-left.png" />
+      </div>
+      <p>
+        <b>{formatDate(weekStart)}</b>
+        <span>a</span>
+        <b>{formatDate(weekEnd)}</b>
+      </p>
+      <div className="next" onClick={() => changeWeek(+7)}>
+        <img alt="Próxima semana" src="/arrow-right.png" />
+      </div>
+    </div>
+  );
+}

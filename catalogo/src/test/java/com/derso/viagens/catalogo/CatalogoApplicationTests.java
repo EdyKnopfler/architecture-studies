73089209236f25f4c9fe676b5e9fd14545b8dbec
Éderson Cássio <edersoncassiolf@outlook.com>
2023-06-12 20:51:16 -0300

    Esqueleto do Initializr com Web, Thymeleaf, Security, Lombok, Data (JPA+ElasticSearch), Session, DevTools (já baixava o Spring inteiro)

---
+++ catalogo/src/test/java/com/derso/viagens/catalogo/CatalogoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.derso.viagens.catalogo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CatalogoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

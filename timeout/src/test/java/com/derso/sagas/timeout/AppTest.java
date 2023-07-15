dae6323deb64a5f47d55a63b4f5732c4321fff9e
Éderson Cássio <edersoncassio1983@gmail.com>
2023-07-15 19:30:45 -0300

    Projeto Maven Quick Start

---
+++ timeout/src/test/java/com/derso/sagas/timeout/AppTest.java
@@ -0,0 +1,20 @@
+package com.derso.sagas.timeout;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}

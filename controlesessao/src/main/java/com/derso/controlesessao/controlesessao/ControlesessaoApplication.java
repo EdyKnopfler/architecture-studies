6b6ce689d8291cb1957d7c7dc1bdd0768c7a7d68
Éderson Cássio <edersoncassio1983@gmail.com>
2023-10-04 20:13:15 -0300

    Esqueleto do Spring Boot com Web, JPA, Integration, Lombok e DevTools

---
+++ controlesessao/src/main/java/com/derso/controlesessao/controlesessao/ControlesessaoApplication.java
@@ -0,0 +1,13 @@
+package com.derso.controlesessao.controlesessao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ControlesessaoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ControlesessaoApplication.class, args);
+	}
+
+}
---
+++ controlesessao/src/test/java/com/derso/controlesessao/controlesessao/ControlesessaoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.derso.controlesessao.controlesessao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ControlesessaoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

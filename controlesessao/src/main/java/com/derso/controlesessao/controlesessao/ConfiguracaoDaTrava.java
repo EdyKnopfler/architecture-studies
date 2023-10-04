99a2d6ac8f65c0801be1b05df96c6fc3994ffe1c
Éderson Cássio <edersoncassio1983@gmail.com>
2023-10-04 20:58:47 -0300

    Configuração da trava dando tempo (supostamente) suficiente para o estado da sessão ser alterado

---
+++ controlesessao/src/main/java/com/derso/controlesessao/controlesessao/ConfiguracaoDaTrava.java
@@ -0,0 +1,23 @@
+package com.derso.controlesessao.controlesessao;
+
+import java.time.Duration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.integration.redis.util.RedisLockRegistry;
+import org.springframework.integration.support.locks.ExpirableLockRegistry;
+
+@Configuration
+public class ConfiguracaoDaTrava {
+	
+	private static final String NOME_TRAVA = "SESSAO_MUTEX";
+	private static final Duration DURACAO_TRAVA = Duration.ofSeconds(30);
+	
+	@Bean(destroyMethod = "destroy")
+	public ExpirableLockRegistry redisLockRegistry(RedisConnectionFactory redisConnectionFactory) {
+	    return new RedisLockRegistry(redisConnectionFactory, NOME_TRAVA, DURACAO_TRAVA.toMillis());
+	}
+
+
+}

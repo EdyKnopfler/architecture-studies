a409b5ca8432b197ca113058cdacbb7930d06317
Éderson Cássio <edersoncassio1983@gmail.com>
2023-10-04 21:16:26 -0300

    Refactor: nome da classe

--- controlesessao/src/main/java/com/derso/controlesessao/controlesessao/ControleSessaoApplication.java
+++ controlesessao/src/main/java/com/derso/controlesessao/controlesessao/ControleSessaoApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ControlesessaoApplication {
+public class ControleSessaoApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ControlesessaoApplication.class, args);
+		SpringApplication.run(ControleSessaoApplication.class, args);
 	}
 
 }

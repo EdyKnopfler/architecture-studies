9ad3d77fff8afd65f151cc9d177180f7097bb03a
Éderson Cássio <edersoncassio1983@gmail.com>
2023-10-07 18:27:45 -0300

    Esqueleto do Spring Starter

---
+++ disparotimeouts/src/main/java/com/derso/disparotimeouts/DisparotimeoutsApplication.java
@@ -0,0 +1,13 @@
+package com.derso.disparotimeouts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DisparotimeoutsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DisparotimeoutsApplication.class, args);
+	}
+
+}

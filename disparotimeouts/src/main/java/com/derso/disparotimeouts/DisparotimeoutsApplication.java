60e7a971e65cf4dee90acaed0b19109964c82ddb
Éderson Cássio <edersoncassio1983@gmail.com>
2023-10-07 18:31:19 -0300

    Habilitando agendamento de tarefas

--- disparotimeouts/src/main/java/com/derso/disparotimeouts/DisparotimeoutsApplication.java
+++ disparotimeouts/src/main/java/com/derso/disparotimeouts/DisparotimeoutsApplication.java
@@ -2,8 +2,10 @@ package com.derso.disparotimeouts;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class DisparotimeoutsApplication {
 
 	public static void main(String[] args) {

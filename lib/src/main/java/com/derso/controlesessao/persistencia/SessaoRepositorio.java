771d03debf8e09e26fee48eb18f7590cbe65fb5a
Éderson Cássio <edersoncassio1983@gmail.com>
2023-10-08 22:13:08 -0300

    Consulta das sessões expiradas

--- lib/src/main/java/com/derso/controlesessao/persistencia/SessaoRepositorio.java
+++ lib/src/main/java/com/derso/controlesessao/persistencia/SessaoRepositorio.java
@@ -1,5 +1,7 @@
 package com.derso.controlesessao.persistencia;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
@@ -10,5 +12,12 @@ public interface SessaoRepositorio extends JpaRepository<Sessao, String> {
 	@Modifying
 	@Query("UPDATE Sessao s SET s.estado = :novoEstado WHERE s.uuid = :uuid")
 	void atualizarEstado(@Param("uuid") String uuid, @Param("novoEstado") EstadoSessao novoEstado);
+	
+	@Query("""
+		SELECT s FROM Sessao s 
+		WHERE s.estado = 'CORRENDO' 
+		AND s.expiracao < CURRENT_TIMESTAMP 
+	""")
+	List<Sessao> sessoesExpiradasCorrendo();
 
 }
